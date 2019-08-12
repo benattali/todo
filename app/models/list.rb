5713ee19027072fefb6b56dc45699b9cd46ea931
Ben Attali <ben.attali8@gmail.com>
2019-08-12 22:35:01 +0300

    sorting the tasks alphabetically

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-	has_many :tasks, inverse_of: :list, dependent: :destroy
+	has_many :tasks, -> { order "description asc" }, inverse_of: :list, dependent: :destroy
 	accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 
 	validates :title, presence: true
