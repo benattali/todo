03ec47059c8ebe72f443eebab6a8190bd9738c41
Ben Attali <ben.attali8@gmail.com>
2019-08-23 13:35:21 +0300

    validations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :lists, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\w+@\D+\.{1}[a-zA-Z]{2,}/ }
+  validates :password, length: { minimum: 6 }
 end
